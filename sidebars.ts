734834bd08020da2b4621405c4fea9ed990b4aa1
Phong Vo <phongvo27ams@gmail.com>
2026-06-18 22:59:27 +0700

    feat: update exercise titles and remove unused index files for black box testing and test automation

--- sidebars.ts
+++ sidebars.ts
@@ -6,9 +6,7 @@ const sidebars: SidebarsConfig = {
     {
       type: 'category',
       label: 'Boundary Value Analysis',
-      link: {type: 'doc', id: 'black-box-testing/boundary-value-analysis/index'},
       items: [
-        'black-box-testing/boundary-value-analysis/index',
         'black-box-testing/boundary-value-analysis/11267/index',
         'black-box-testing/boundary-value-analysis/50944/index',
       ],
@@ -16,9 +14,7 @@ const sidebars: SidebarsConfig = {
     {
       type: 'category',
       label: 'Equivalence Class Partitioning',
-      link: {type: 'doc', id: 'black-box-testing/equivalence-class-partitioning/index'},
       items: [
-        'black-box-testing/equivalence-class-partitioning/index',
         'black-box-testing/equivalence-class-partitioning/47382/index',
       ],
     },
@@ -27,10 +23,8 @@ const sidebars: SidebarsConfig = {
     'test-automation/index',
     {
       type: 'category',
-      label: 'Pending Topics',
-      link: {type: 'doc', id: 'test-automation/pending-topics/index'},
+      label: 'Pending',
       items: [
-        'test-automation/pending-topics/index',
         'test-automation/pending-topics/00542/index',
         'test-automation/pending-topics/73752/index',
       ],
